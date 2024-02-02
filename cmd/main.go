415d922babd8c3de05b733dab7f128d9dc7dcd68
hwoj <hwojnicki41@gmail.com>
2024-02-01 23:11:22 -0800

    renamed pipe file to moisturepipe.go and call it from main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"plant-monitoring-server/internal/moisturepipe"
+	"sync"
+)
+
 func main() {
-	
+	var wait_group sync.WaitGroup
+
+	var moisture_value float32
+
+	wait_group.Add(1)
+	go moisturepipe.ReadFromMoisturePipe(&wait_group, &moisture_value)
+
 }
---
+++ internal/moisturepipe/moisturepipe.go
@@ -0,0 +1,7 @@
+package moisturepipe
+
+import "sync"
+
+func ReadFromMoisturePipe(wait_group *sync.WaitGroup, moisture_value *float32) {
+
+}
