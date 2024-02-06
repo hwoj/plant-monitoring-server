072e4a551ef8d01f8c17d933015e692b6f4574b7
hwoj <hwojnicki41@gmail.com>
2024-02-06 10:51:03 -0800

    wrote preliminary namedpipe code. started some of the websocket code

--- internal/moisturepipe/moisturepipe.go
+++ internal/moisturepipe/moisturepipe.go
@@ -1,7 +1,41 @@
 package moisturepipe
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+	"syscall"
+)
 
 func ReadFromMoisturePipe(wait_group *sync.WaitGroup, moisture_value *float32) {
+
+	pipeName := "moisturepipe"
+
+	_ = os.Remove(pipeName)
+
+	err := syscall.Mkfifo(pipeName, 0666)
+
+	if err != nil {
+		fmt.Println("Error from creating pipe:", err)
+
+	}
+
+	namedPipe, err := os.OpenFile(pipeName, os.O_RDONLY, os.ModeNamedPipe)
+
+	if err != nil {
+		fmt.Println("Error from opening pipe:", err)
+	}
+
+	defer namedPipe.Close()
+
+	buf := make([]byte, 50)
+	for {
+		bytes, err := namedPipe.Read(buf)
+
+		if err != nil {
+			fmt.Printf("Error reading %d from named pipe: %s", bytes, err)
+		}
+		*moisture_value = 2.2 + float32(bytes)
+	}
 
 }
--- internal/websocket/websocket.go
+++ internal/websocket/websocket.go
@@ -1,1 +1,21 @@
 package websocket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	clients = make(map[*websocket.Conn]bool)
+)
+
+func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+
+}
